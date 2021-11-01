fd6147385d51457ea59cfa2a6f01ed0f7c76f97f
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-11-01 17:26:18 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,22 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+    //constructor(props) {
+    //    super(props);
+    //}
+
+
+    mCoor = (event) => {
+        const mArray = [event.clientX, event.clientY]
+        this.props.onReceiveCoordinates(mArray)
+    }
+
+    render() {
+    return (
+        <button
+        onClick={this.mCoor}>
+        </button>
+    )
+}
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+    dClick = (event) => {
+        event.persist()
+        setTimeout(() => {
+            this.props.onDelayedClick(event)
+        }, this.props.delay)
+    }
+    
+    render() {
+    return (
+        <button
+        onClick={this.dClick}>    
+        </button>
+    )
+}
+}
